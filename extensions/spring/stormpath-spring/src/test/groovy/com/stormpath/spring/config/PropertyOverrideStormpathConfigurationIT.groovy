c679f0d3df1bafc39b8d960aa89731ab69f66370
Micah Silverman <micah@afitnerd.com>
2016-08-03 01:24:48 -0700

    Switched test tenants and updated PropertyOverrideStormpathConfigurationIT for new test tenant

--- extensions/spring/stormpath-spring/src/test/groovy/com/stormpath/spring/config/PropertyOverrideStormpathConfigurationIT.groovy
+++ extensions/spring/stormpath-spring/src/test/groovy/com/stormpath/spring/config/PropertyOverrideStormpathConfigurationIT.groovy
@@ -63,7 +63,7 @@ class PropertyOverrideStormpathConfigurationIT extends AbstractTestNGSpringConte
         //assert app href override worked as expected:
         def expected =
             System.getenv("STORMPATH_TEST_APPLICATION_HREF") ?:
-            'https://api.stormpath.com/v1/applications/TAuH8ZMkA20STGvXf8iB3'
+            'https://api.stormpath.com/v1/applications/4DeLl67pB8MZGx8xL8PnPd'
         assertEquals application.href, expected
     }
 }
